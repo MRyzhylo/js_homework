8992835352b14a77c478e122d7124481276da851
MRyzhylo <playa77777@gmail.com>
2019-04-30 15:23:14 +0300

    task 4 create constructor

--- task_4/main.js
+++ task_4/main.js
@@ -6,19 +6,26 @@ function blockPosition (nameId) {
     this.btnRight = document.getElementById('right');
     this.btnUp = document.getElementById('up');
     this.btnDown = document.getElementById('down');
-    this.winHeigt = document.documentElement.clientHeight;
-    this.winWeigt = document.documentElement.clientWidth;
-    this.scrllDwn = function () {
-        if(window.pageYOffset < this.winHeigt){
-            window.scrollBy(0, 10);
-            setTimeout(scroll, 0);
-            return scrllDwn()
-        }
-        if(window.pageYOffset > this.winHeigt){
-            window.scrollTo(0, this.winHeigt);
-        }
-    }
-    this.btnDown.onclick = this.scrllDwn;
+    
+    this.moveUp = function () {
+        this.container.className = 'btn-up';
+    }.bind(this);
+    this.btnUp.onclick = this.moveUp;
+
+    this.moveDown = function () {
+        this.container.className = 'btn-down';
+    }.bind(this);
+    this.btnDown.onclick = this.moveDown;
+
+    this.moveLeft = function () {
+        this.container.className = 'btn-left';
+    }.bind(this);
+    this.btnLeft.onclick = this.moveLeft;
+
+    this.moveRight = function () {
+        this.container.className = 'btn-right';
+    }.bind(this);
+    this.btnRight.onclick = this.moveRight;
 }
 
 var di = new blockPosition('main_block');
