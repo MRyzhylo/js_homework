fe41d2ad0e9bcef574190a7ee07495232b83aacf
MRyzhylo <playa77777@gmail.com>
2019-05-07 18:34:32 +0300

    task 5 version 1

---
+++ task_5/main.js
@@ -0,0 +1,31 @@
+'use strict'
+
+var arr = ['Kyiv', 'Kharkiv', 'Dnipro', 'Odessa', 'Poltava', 'Lviv'];
+
+var box = document.getElementById('main_block');
+var secondBox = document.getElementById('second_block'); 
+var ul = document.createElement('ul');
+var ul1 = document.createElement('ul');
+box.appendChild(ul);
+secondBox.appendChild(ul1);
+
+for (var name of arr) {
+    var li = document.createElement('li');
+    ul.appendChild(li);
+    li.innerHTML = name;
+    var button = document.createElement('button');
+    button.innerHTML = 'x';
+    li.appendChild(button);
+    button.onclick = elemReplace;
+}
+
+function elemReplace () {
+    if(this.li === this.name) {
+        ul1.appendChild(this.li);
+    }
+    if(this.li !== this.name) {
+        ul.appendChild(li);
+    }
+}
+
+// console.log();
