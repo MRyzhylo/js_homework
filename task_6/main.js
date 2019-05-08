a47107fddbe2879f9bdf07fe4359701eb348bb43
MRyzhylo <playa77777@gmail.com>
2019-05-08 15:09:20 +0300

    complete task 6

---
+++ task_6/main.js
@@ -0,0 +1,108 @@
+'use strict'
+
+var arr = [];
+
+function firstForm () {
+    var container = document.getElementById('container');
+    var firstDiv = document.createElement('div');
+    firstDiv.id = 'main_block';
+    var firstInput = document.createElement('input');
+    var secondInput = document.createElement('input');
+    firstInput.type = Text;
+    secondInput.type = Text;
+    var button = document.createElement('button');
+    button.innerHTML = 'Next';
+    firstDiv.appendChild(firstInput);
+    firstDiv.appendChild(secondInput);
+    firstDiv.appendChild(button);
+    container.appendChild(firstDiv);
+    button.onclick = function () {
+        arr.push(firstInput.value, secondInput.value);
+        firstDiv.style.display = 'none';
+        SecondForm();
+    }
+}
+firstForm();
+function SecondForm () {
+    var container = document.getElementById('container');
+    var secondDiv = document.createElement('div');
+    secondDiv.id = 'second_block';
+    var firstInput = document.createElement('input');
+    var secondInput = document.createElement('input');
+    firstInput.type = Text;
+    secondInput.type = Text;
+    var button = document.createElement('button');
+    button.innerHTML = 'Next';
+    secondDiv.appendChild(firstInput);
+    secondDiv.appendChild(secondInput);
+    secondDiv.appendChild(button);
+    container.appendChild(secondDiv);
+    button.onclick = function () {
+        arr.push(firstInput.value, secondInput.value);
+        secondDiv.style.display = 'none';
+        ThirdForm();
+    }
+}
+function ThirdForm () {
+    var container = document.getElementById('container');
+    var thirdDiv = document.createElement('div');
+    thirdDiv.id = 'third_block';
+    var firstInput = document.createElement('input');
+    var secondInput = document.createElement('input');
+    firstInput.type = Text;
+    secondInput.type = Text;
+    var button = document.createElement('button');
+    button.innerHTML = 'Next';
+    thirdDiv.appendChild(firstInput);
+    thirdDiv.appendChild(secondInput);
+    thirdDiv.appendChild(button);
+    container.appendChild(thirdDiv);
+    button.onclick = function () {
+        arr.push(firstInput.value, secondInput.value);
+        thirdDiv.style.display = 'none';
+        finalForm();
+    }
+}
+function finalForm () {
+    var container = document.getElementById('container');
+    var finalDiv = document.createElement('div');
+    finalDiv.id = 'final_block';
+    var button = document.createElement('button');
+    button.innerHTML = 'Send Form';
+    container.appendChild(finalDiv);
+
+    for(var name of arr) {
+        var p = document.createElement('p');
+        finalDiv.appendChild(p)
+        p.innerHTML = name;
+    }
+
+    function sendData () {
+        alert('Data send sucsesfully!');
+    }
+    finalDiv.appendChild(button);
+    button.onclick = sendData;
+}
+
+console.log(arr);
+
+
+
+// function showInput () {
+//     function firstForm () {
+//         this.body = document.getElementsByTagName('body');
+//         this.firstDiv = document.createElement('div');
+//         this.firstDiv.id = 'main_block';
+//         this.firstInput = document.createElement('input');
+//         this.secondInput = document.createElement('input');
+//         this.firstInput.type = Text;
+//         this.secondInput.type = Text;
+//         this.button = document.createElement('button');
+//         this.firstDiv.appenChild(this.firstInput);
+//         this.firstDiv.appenChild(this.secondInput);
+//         this.firstDiv.appenChild(this.secondInput);
+//         this.body.appenChild(this.firstDiv);
+//     }
+//     firstForm();
+// }
+
